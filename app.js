25a73cfc585a0d207971ead07b36dd11696d9911
julio Tuozzo <jtuozzo@hotmail.com>
2022-06-11 19:18:58 -0300

    Versión inicial

---
+++ app.js
@@ -0,0 +1,28 @@
+// Librerías, módulos, variables, etc.
+
+const express = require('express');
+
+const app = express();
+const puerto = 3000;
+
+
+// Configuración del servidor
+
+app.use('/static', express.static(__dirname + '/public/' ))
+app.use('/imagenes', express.static(__dirname + '/public/images' )) 
+
+// Rutas
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html')
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html')
+})
+
+// Levantar el server
+
+app.listen(puerto, ()=> {
+    console.log("servidor corriendo en el puerto " + puerto);
+})
