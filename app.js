1916790af684267efbae89f6923234a3cbdb80fb
julio Tuozzo <jtuozzo@hotmail.com>
2022-06-11 23:15:06 -0300

    Formulario de login hecho

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ app.get('/register', (req, res) => {
     res.sendFile(__dirname + '/views/register.html')
 })
 
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html')
+})
+
 // Levantar el server
 
 app.listen(puerto, ()=> {
